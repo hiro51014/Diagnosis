4dc546010af4c29dd6de1b96513fb6b69b5a3d79
reyos0701 <kamereon0701@softbank.ne.jp>
2020-07-13 16:12:27 +0900

    tinにしました

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    LaravelUNTI
+                    Laraveltintin
                 </div>
 
                 <div class="links">
